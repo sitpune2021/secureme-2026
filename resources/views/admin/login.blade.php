fde4e6f286c3a2a87ddc5b1b2d6a160c3610d363
mayurkakde <sitpune2021>
2026-03-06 15:57:13 +0530

    commented code removed

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -29,7 +29,7 @@
                         class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h4>Admin Loginvvvvvvvv</h4>
+                                <h4>Admin Login</h4>
                             </div>
                             <div class="card-body">
                                 {{-- ✅ Show Validation or Login Errors --}}
